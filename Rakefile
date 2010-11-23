0820a0b8a09afe765cf7cc1b4e06e93c446d9c1b
Steven! Lorenzen <steven@nuclearsandwich.com>
2010-11-23 13:11:36 -0800

    Finalized design, implemented mouseClicked().

--- Rakefile
+++ Rakefile
@@ -1,10 +1,23 @@
-require 'raven'
 require 'rake/clean'
 
 SRC = FileList["src/*.java"]
 CLEAN.include("bin/*.class")
 
-javac 'compile' do |t|
-	t.build_path << 'src/'
+JCLASSES = %w{Mancala MancalaGUI MancalaCLI Board}
+
+JCLASSES.each do |jclass|
+	file "bin/#{jclass}.class" => [ "src/#{jclass}.java"] do
+		sh "javac -cp bin/ -d bin/ src/#{jclass}.java"
+	end
+end
+
+file "bin/MancalaGUI.class" => ["bin/Mancala.class"]
+file "bin/MancalaCLI.class" => ["bin/Mancala.class"]
+
+task :main => "bin/MancalaGUI.class"
+task :default => :main
+
+task :run => [:main] do
+	sh "java -cp bin/ MancalaGUI"
 end
 
--- src/MancalaGUI.java
+++ src/MancalaGUI.java
@@ -2,7 +2,7 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class MancalaGUI
+public class MancalaGUI implements MouseListener
 {
 	static Mancala game;
 	static int stoneCount = 3;
@@ -136,5 +136,21 @@ public class MancalaGUI
          }
       };
    }
-	
+
+	public void mouseClicked(MouseEvent e)
+	{
+		rects = board.getPitRectangles();
+		for (int row = 0; row < Mancala.N_PLAYERS; row++)
+			for (int col = 0; col < Mancala.BOARD_LENGTH; col++)
+			{
+				if rect[row][col].contains(e.getPoint())
+					try { game.move(row,col); }
+					catch (IllegalArgumentException ex)
+					{
+						JOptionPane.showMessageDialog(this, ex.getMessage(),
+								"Invalid Move", JOptionPane.WARNING_MESSAGE);
+					}
+			}
+	}
 }
+
