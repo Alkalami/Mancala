b73dee35f6455872d7cff14f5d5a2a566c7b4996
Steven! Lorenzen <steven@nuclearsandwich.com>
2010-12-04 01:36:24 -0800

    Stone Distribution is suckier but can see them everywhere.

--- src/GeomLayout.java
+++ src/GeomLayout.java
@@ -34,12 +34,17 @@ public class GeomLayout extends BoardLayout
 			// Draw the stones.
 		for (int r = 0; r < pits.length; r++)
 		{
-			// Draw into mancalas
+			for (int stone = 0; stone < mancalas[r]; stone++)
+			{
+				g.draw(rotaryStone((int)mRects[r].getX(),
+								(int)mRects[r].getY(), (int)mRects[r].getWidth(),
+								(int)mRects[r].getHeight()));
+			}
 			for (int c = 0; c < pits[r].length; c++)
 				for (int stone = pits[r][c]; stone > 0; stone--)
 					g.draw(rotaryStone((int)pitRects[r][c].getX(),
 								(int)pitRects[r][c].getY(), (int)pitRects[r][c].getWidth(),
-								(int)pitRects[r][c].getHeight(), stone));
+								(int)pitRects[r][c].getHeight()));
 		}
 
 	}
@@ -48,13 +53,19 @@ public class GeomLayout extends BoardLayout
 	public void setSize(int w, int h)
 	{
 		super.setSize(w,h);
-		mRects[0] = new Rectangle2D.Double(0, 0 , width / 8, height);
-		mRects[1] = new Rectangle2D.Double(width * 7 / 8, 0, width / 8, height);
-			for (int r = 0; r < nPlayers; r++)
-				for (int c = 0; c < boardLength; c++)
-					pitRects[r][c] = new Rectangle2D.Double(width / 8 * (c + 1),
-							r * height / 2, width / 8, height / 2);
-	}
+		mRects[0] = new Rectangle2D.Double(width * 7 / 8, 0, width / 8, height);
+		mRects[1] = new Rectangle2D.Double(0, 0 , width / 8, height);
+		for (int c = 0; c < boardLength; c++)
+			pitRects[1][c] = new Rectangle2D.Double(width / 8 * (c + 1),
+					0, width / 8, height / 2);
+		int s = boardLength - 1; /* Hack var to reverse direction */
+		for (int c = 0; c < boardLength; c++)
+		{
+			pitRects[0][c] = new Rectangle2D.Double(width / 8 * (c + 1),
+					height / 2, width / 8, height / 2);
+			--s;
+		}
+}
 
 	/**
 	 * Creates an ellipse to use as a stone.
@@ -65,17 +76,16 @@ public class GeomLayout extends BoardLayout
 	 * @param n an integer to aid in seeding the random number generator
 	 */
 	private Ellipse2D.Double rotaryStone(int xorigin, int yorigin,
-			int boxwidth, int boxheight, int n)
+			int boxwidth, int boxheight)
 	{
-		rand.setSeed((xorigin + 1) * (yorigin + 1) * n);
 		double a = rand.nextDouble() * 2 * Math.PI;
-		int stoneR = width / 32;
+		int stoneR = boxwidth / 8;
 		int ringCenterX = xorigin + boxwidth / 2;
 		int ringCenterY = yorigin + boxheight / 2;
 		int stoneCenterX = (int)(ringCenterX + stoneR * Math.cos(a));
 		int stoneCenterY = (int)(ringCenterY + stoneR * Math.sin(a));
 		System.out.println("DEBUG: R: " + stoneR + " stoneX: " + stoneCenterX +
-				" stoneY: " + stoneCenterY + " n: " + n + " a: " + a);
+				" stoneY: " + stoneCenterY + " a: " + a);
 		return new Ellipse2D.Double(stoneCenterX - stoneR * Math.sqrt(2),
 				stoneCenterY - stoneR * Math.sqrt(2), stoneR * 2, stoneR * 2);
 	}
